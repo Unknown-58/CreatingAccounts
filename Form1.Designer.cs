e775e442f2349a0a125d8604119ba18500325098
Pavel <pasha.maksimov.1974@mail.ru>
2025-02-27 00:46:31 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Test
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.TextBox1_Label = new System.Windows.Forms.TextBox();
+            this.NameText = new System.Windows.Forms.Label();
+            this.FirstName_Label = new System.Windows.Forms.TextBox();
+            this.LastName_Label = new System.Windows.Forms.TextBox();
+            this.Login_Label = new System.Windows.Forms.TextBox();
+            this.ComboBox_Domain = new System.Windows.Forms.ComboBox();
+            this.Email_Label = new System.Windows.Forms.TextBox();
+            this.Password_Label = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
+            this.Domain = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button1.Location = new System.Drawing.Point(151, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // TextBox1_Label
+            // 
+            this.TextBox1_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.TextBox1_Label.Location = new System.Drawing.Point(12, 29);
+            this.TextBox1_Label.Name = "TextBox1_Label";
+            this.TextBox1_Label.Size = new System.Drawing.Size(220, 21);
+            this.TextBox1_Label.TabIndex = 1;
+            // 
+            // NameText
+            // 
+            this.NameText.AutoSize = true;
+            this.NameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.NameText.Location = new System.Drawing.Point(8, 11);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(100, 15);
+            this.NameText.TabIndex = 2;
+            this.NameText.Text = "Фамилия и Имя";
+            // 
+            // FirstName_Label
+            // 
+            this.FirstName_Label.BackColor = System.Drawing.SystemColors.Control;
+            this.FirstName_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.FirstName_Label.Location = new System.Drawing.Point(13, 69);
+            this.FirstName_Label.Name = "FirstName_Label";
+            this.FirstName_Label.ReadOnly = true;
+            this.FirstName_Label.Size = new System.Drawing.Size(119, 20);
+            this.FirstName_Label.TabIndex = 3;
+            this.FirstName_Label.Click += new System.EventHandler(this.FirstName_Label_Click);
+            // 
+            // LastName_Label
+            // 
+            this.LastName_Label.BackColor = System.Drawing.SystemColors.Control;
+            this.LastName_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.LastName_Label.Location = new System.Drawing.Point(162, 69);
+            this.LastName_Label.Name = "LastName_Label";
+            this.LastName_Label.ReadOnly = true;
+            this.LastName_Label.Size = new System.Drawing.Size(125, 20);
+            this.LastName_Label.TabIndex = 4;
+            this.LastName_Label.Click += new System.EventHandler(this.LastName_Label_Click);
+            // 
+            // Login_Label
+            // 
+            this.Login_Label.BackColor = System.Drawing.SystemColors.Control;
+            this.Login_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Login_Label.Location = new System.Drawing.Point(12, 118);
+            this.Login_Label.Name = "Login_Label";
+            this.Login_Label.ReadOnly = true;
+            this.Login_Label.Size = new System.Drawing.Size(143, 20);
+            this.Login_Label.TabIndex = 5;
+            this.Login_Label.Click += new System.EventHandler(this.Login_Label_Click);
+            // 
+            // ComboBox_Domain
+            // 
+            this.ComboBox_Domain.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_Domain.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.ComboBox_Domain.FormattingEnabled = true;
+            this.ComboBox_Domain.Location = new System.Drawing.Point(162, 118);
+            this.ComboBox_Domain.Name = "ComboBox_Domain";
+            this.ComboBox_Domain.Size = new System.Drawing.Size(126, 21);
+            this.ComboBox_Domain.TabIndex = 6;
+            // 
+            // Email_Label
+            // 
+            this.Email_Label.BackColor = System.Drawing.SystemColors.Control;
+            this.Email_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Email_Label.Location = new System.Drawing.Point(12, 157);
+            this.Email_Label.Name = "Email_Label";
+            this.Email_Label.ReadOnly = true;
+            this.Email_Label.Size = new System.Drawing.Size(182, 20);
+            this.Email_Label.TabIndex = 7;
+            this.Email_Label.Click += new System.EventHandler(this.Email_Label_Click);
+            // 
+            // Password_Label
+            // 
+            this.Password_Label.BackColor = System.Drawing.SystemColors.Control;
+            this.Password_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Password_Label.Location = new System.Drawing.Point(12, 196);
+            this.Password_Label.Name = "Password_Label";
+            this.Password_Label.ReadOnly = true;
+            this.Password_Label.Size = new System.Drawing.Size(182, 20);
+            this.Password_Label.TabIndex = 8;
+            this.Password_Label.Click += new System.EventHandler(this.Password_Label_Click);
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Email.Location = new System.Drawing.Point(8, 141);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(32, 13);
+            this.Email.TabIndex = 9;
+            this.Email.Text = "Email";
+            // 
+            // Domain
+            // 
+            this.Domain.AutoSize = true;
+            this.Domain.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Domain.Location = new System.Drawing.Point(159, 102);
+            this.Domain.Name = "Domain";
+            this.Domain.Size = new System.Drawing.Size(42, 13);
+            this.Domain.TabIndex = 10;
+            this.Domain.Text = "Домен";
+            // 
+            // Login
+            // 
+            this.Login.AutoSize = true;
+            this.Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Login.Location = new System.Drawing.Point(9, 102);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(33, 13);
+            this.Login.TabIndex = 11;
+            this.Login.Text = "Login";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Password.Location = new System.Drawing.Point(8, 180);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(53, 13);
+            this.Password.TabIndex = 12;
+            this.Password.Text = "Password";
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.FirstName.Location = new System.Drawing.Point(10, 53);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(54, 13);
+            this.FirstName.TabIndex = 13;
+            this.FirstName.Text = "FirstName";
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSize = true;
+            this.LastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.LastName.Location = new System.Drawing.Point(159, 53);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(55, 13);
+            this.LastName.TabIndex = 14;
+            this.LastName.Text = "LastName";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 286);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Domain);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Password_Label);
+            this.Controls.Add(this.Email_Label);
+            this.Controls.Add(this.ComboBox_Domain);
+            this.Controls.Add(this.Login_Label);
+            this.Controls.Add(this.LastName_Label);
+            this.Controls.Add(this.FirstName_Label);
+            this.Controls.Add(this.NameText);
+            this.Controls.Add(this.TextBox1_Label);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TextBox1_Label;
+        private System.Windows.Forms.Label NameText;
+        private System.Windows.Forms.TextBox FirstName_Label;
+        private System.Windows.Forms.TextBox LastName_Label;
+        private System.Windows.Forms.TextBox Login_Label;
+        private System.Windows.Forms.ComboBox ComboBox_Domain;
+        private System.Windows.Forms.TextBox Email_Label;
+        private System.Windows.Forms.TextBox Password_Label;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Domain;
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label LastName;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Добавляем домены
+            ComboBox_Domain.Items.AddRange(new string[] { "@mail.ru", "@gmail.com", "@yandex.ru", "@outlook.com" });
+            ComboBox_Domain.SelectedIndex = ComboBox_Domain.Items.Count > 0 ? 0 : -1; // Устанавливаем первый элемент по умолчанию
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string input = TextBox1_Label.Text.Trim();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Введите Фамилию и Имя через пробел.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] nameParts = input.Split(' ');
+            if (nameParts.Length != 2)
+            {
+                MessageBox.Show("Введите только Фамилию и Имя через один пробел.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string lastName = nameParts[0];
+            string firstName = nameParts[1];
+
+            string transliteredLastName = Transliterate(lastName);
+            string transliteredFirstName = Transliterate(firstName);
+
+            FirstName_Label.Text = transliteredLastName;
+            LastName_Label.Text = transliteredFirstName;
+
+            string login = $"{transliteredFirstName[0].ToString().ToLower()}.{transliteredLastName.ToLower()}";
+            Login_Label.Text = login;
+
+            if (ComboBox_Domain.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите домен для email.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string domain = ComboBox_Domain.SelectedItem.ToString();
+            string email = $"{login}{domain}";
+            Email_Label.Text = email;
+
+            string password = GeneratePassword(16);
+            Password_Label.Text = password;
+        }
+
+        private string Transliterate(string text)
+        {
+            Dictionary<char, string> translitMap = new Dictionary<char, string>() {
+                {'а', "a"}, {'б', "b"}, {'в', "v"}, {'г', "g"}, {'д', "d"}, {'е', "e"},
+                {'ё', "yo"}, {'ж', "zh"}, {'з', "z"}, {'и', "i"}, {'й', "y"}, {'к', "k"},
+                {'л', "l"}, {'м', "m"}, {'н', "n"}, {'о', "o"}, {'п', "p"}, {'р', "r"},
+                {'с', "s"}, {'т', "t"}, {'у', "u"}, {'ф', "f"}, {'х', "kh"}, {'ц', "ts"},
+                {'ч', "ch"}, {'ш', "sh"}, {'щ', "sch"}, {'ъ', ""}, {'ы', "y"}, {'ь', ""},
+                {'э', "e"}, {'ю', "yu"}, {'я', "ya"},
+                {'А', "A"}, {'Б', "B"}, {'В', "V"}, {'Г', "G"}, {'Д', "D"}, {'Е', "E"},
+                {'Ё', "Yo"}, {'Ж', "Zh"}, {'З', "Z"}, {'И', "I"}, {'Й', "Y"}, {'К', "K"},
+                {'Л', "L"}, {'М', "M"}, {'Н', "N"}, {'О', "O"}, {'П', "P"}, {'Р', "R"},
+                {'С', "S"}, {'Т', "T"}, {'У', "U"}, {'Ф', "F"}, {'Х', "Kh"}, {'Ц', "Ts"},
+                {'Ч', "Ch"}, {'Ш', "Sh"}, {'Щ', "Sch"}, {'Ъ', ""}, {'Ы', "Y"}, {'Ь', ""},
+                {'Э', "E"}, {'Ю', "Yu"}, {'Я', "Ya"}
+            };
+
+            StringBuilder result = new StringBuilder();
+            foreach (char c in text)
+            {
+                if (translitMap.ContainsKey(c))
+                    result.Append(translitMap[c]);
+                else
+                    result.Append(c);
+            }
+
+            return result.ToString();
+        }
+
+        private string GeneratePassword(int length)
+        {           
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+";
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(validChars.Length);
+                password.Append(validChars[index]);
+            }
+
+            return password.ToString();
+        }
+
+        private void FirstName_Label_Click(object sender, EventArgs e) => Clipboard.SetText(FirstName_Label.Text);
+        private void LastName_Label_Click(object sender, EventArgs e) => Clipboard.SetText(LastName_Label.Text);
+        private void Login_Label_Click(object sender, EventArgs e) => Clipboard.SetText(Login_Label.Text);
+        private void Email_Label_Click(object sender, EventArgs e) => Clipboard.SetText(Email_Label.Text);
+        private void Password_Label_Click(object sender, EventArgs e) => Clipboard.SetText(Password_Label.Text);
+
+    }
+}
